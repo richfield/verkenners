9bf64f771f02b2f3f4ecce93d02aab634445e169
Richard ten Velde <richard@ten-velde.com>
2026-09-14 12:55:58 +0200

    add new icons

--- vite.config.ts
+++ vite.config.ts
@@ -7,67 +7,31 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico'],
+      includeAssets: ['icons/favicon.ico', 'icons/apple-touch-icon.png'],
       manifest: {
         name: 'Verkenners',
         short_name: 'Verkenners',
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'web-app-manifest-192x192.png',
+            src: 'icons/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'web-app-manifest-512x512.png',
+            src: 'icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: 'maskable_icon_x192.png',
+            src: 'icons/icon-192-maskable.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'maskable',
           },
           {
-            src: 'maskable_icon_x512.png',
+            src: 'icons/icon-512-maskable.png',
             sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-          {
-            src: 'maskable_icon_x128.png',
-            sizes: '128x128',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-          {
-            src: 'maskable_icon_x384.png',
-            sizes: '384x384',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-          {
-            src: 'maskable_icon_x96.png',
-            sizes: '96x96',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-          {
-            src: 'maskable_icon_x72.png',
-            sizes: '72x72',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-          {
-            src: 'maskable_icon_x48.png',
-            sizes: '48x48',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-          {
-            src: 'maskable_icon.png',
-            sizes: 'any',
             type: 'image/png',
             purpose: 'maskable',
           },
